1b79b39b92a8b2ab508914b30ecddee0baf3e3ee
Tobi Olanrewaju <tobiolanrewaju@klarrys-MacBook-Pro.local>
2019-07-25 23:55:38 +0100

    Added google authentcation strategy

--- src/server.js
+++ src/server.js
@@ -5,6 +5,8 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import logger from 'morgan';
 import 'dotenv/config';
+import passport from 'passport';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
 
 const app = express();
 
@@ -12,9 +14,35 @@ app.use(logger('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('*', (req, res) => res.status(200).send({
-  message: 'Welcome to the application.',
-}));
+// Client ID: 
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: '/auth/google/callback'
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  ) 
+);
+
+app.get('/', (req, res) => {
+  res.send({bye: 'buddy'});
+});
+
+app.get('/auth/google', 
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
+app.get('/auth/google/callback', (req, res) => {
+    let { code } = req.query;
+    console.log("Code: ", code);
+    res.send({code: code});
+})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server up and running on port ${PORT}`));
